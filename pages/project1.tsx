e3a5c51fb3fafc1cd287b249c83fc9d6086bc642
Shamsheer Singh <shamsheerinder@gmail.com>
2022-11-08 22:06:59 -0400

    Added changes to project1

--- pages/project1.tsx
+++ pages/project1.tsx
@@ -13,10 +13,12 @@ const Project1: NextPage = () => {
   return (
     <div>
       <Image src={projectImage} />
-      <div className="mt-4">
-      Developed a platform for online listing of places, primarily based on a customized map that divides 7 continents and allows users to select places based on a specific area and book the location. 
-      Integrated Payment gateways such as PayPal and Stripe with 4 modules to allow users to make payments while reserving rooms. 
-      Optimized the experience of the customer support system by 20% using a chatbot constructed in AWS Lex.
+      <div className="mt-4">      
+         <ul>
+          <li>- Developed a platform for online listing of places, primarily based on a customized map that divides 7 continents and allows users to select places based on a specific area and book the location. </li>
+          <li>- Integrated Payment gateways such as PayPal and Stripe with 4 modules to allow users to make payments while reserving rooms.</li>
+           <li>- Optimized the experience of the customer support system by 20% using a chatbot constructed in AWS Lex.</li>
+        </ul>
       </div>
       <div className="mt-6">
         <Link href="/">
